d131ca128ff37d49291b0eff25c201a88a2eea10
sharmaAshish-dev <60459068+sharmaAshish-dev@users.noreply.github.com>
2022-10-22 00:12:00 +0530

    Setup mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("Hello, World");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT | 3001;
+
+const app = express();
+
+const DB =
+  "mongodb+srv://ashish:AshishMonDB@doccluster.ctz2ktx.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`server running at port ${PORT}`);
+});
